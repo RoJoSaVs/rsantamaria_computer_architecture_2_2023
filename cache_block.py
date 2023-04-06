ed88cef17e6c002be25a75e75857e2efddf3cc3f
RoJoSaVs <ronnyjosv259@outlook.es>
2023-04-06 08:16:58 -0600

    Read miss alert implemented

--- cache_block.py
+++ cache_block.py
@@ -116,82 +116,212 @@ class CacheBlock():
 
 
 	def updateCacheGui(self):
-		self.p0B0.config(state = NORMAL)
+		self.p0B0.config(state = NORMAL, foreground = "#05fa42")
 		self.p0B0.delete("1.0", END)
 		self.p0B0.insert(END, "status: " + cacheModelMatrix[0][0].getStatus() +"\naddress: " + cacheModelMatrix[0][0].getAddress() + "\ndata: " + cacheModelMatrix[0][0].getValue())
+		if(cacheModelMatrix[0][0].getStatus() == "E"):
+			self.p0B0.config(foreground = "red")
 		self.p0B0.config(state = DISABLED)
-		 
-		self.p0B1.config(state = NORMAL)
+
+		self.p0B1.config(state = NORMAL, foreground = "#05fa42")
 		self.p0B1.delete("1.0", END)
 		self.p0B1.insert(END, "status: " + cacheModelMatrix[0][1].getStatus() +"\naddress: " + cacheModelMatrix[0][1].getAddress() + "\ndata: " + cacheModelMatrix[0][1].getValue())
+		if(cacheModelMatrix[0][1].getStatus() == "E"):
+			self.p0B1.config(foreground = "red")
 		self.p0B1.config(state = DISABLED)
-		 
-		self.p0B2.config(state = NORMAL)
+		
+		self.p0B2.config(state = NORMAL, foreground = "#05fa42")
 		self.p0B2.delete("1.0", END)
 		self.p0B2.insert(END, "status: " + cacheModelMatrix[0][2].getStatus() +"\naddress: " + cacheModelMatrix[0][2].getAddress() + "\ndata: " + cacheModelMatrix[0][2].getValue())
+		if(cacheModelMatrix[0][2].getStatus() == "E"):
+			self.p0B2.config(foreground = "red")
 		self.p0B2.config(state = DISABLED)
-		 
-		self.p0B3.config(state = NORMAL)
+
+		self.p0B3.config(state = NORMAL, foreground = "#05fa42")
 		self.p0B3.delete("1.0", END)
 		self.p0B3.insert(END, "status: " + cacheModelMatrix[0][3].getStatus() +"\naddress: " + cacheModelMatrix[0][3].getAddress() + "\ndata: " + cacheModelMatrix[0][3].getValue())
+		if(cacheModelMatrix[0][3].getStatus() == "E"):
+			self.p0B3.config(foreground = "red")
 		self.p0B3.config(state = DISABLED)
-		 
-		self.p1B0.config(state = NORMAL)
+
+		self.p1B0.config(state = NORMAL, foreground = "#05fa42")
 		self.p1B0.delete("1.0", END)
 		self.p1B0.insert(END, "status: " + cacheModelMatrix[1][0].getStatus() +"\naddress: " + cacheModelMatrix[1][0].getAddress() + "\ndata: " + cacheModelMatrix[1][0].getValue())
+		if(cacheModelMatrix[1][0].getStatus() == "E"):
+			self.p1B0.config(foreground = "red")
 		self.p1B0.config(state = DISABLED)
-		 
-		self.p1B1.config(state = NORMAL)
+
+		self.p1B1.config(state = NORMAL, foreground = "#05fa42")
 		self.p1B1.delete("1.0", END)
 		self.p1B1.insert(END, "status: " + cacheModelMatrix[1][1].getStatus() +"\naddress: " + cacheModelMatrix[1][1].getAddress() + "\ndata: " + cacheModelMatrix[1][1].getValue())
+		if(cacheModelMatrix[1][1].getStatus() == "E"):
+			self.p1B1.config(foreground = "red")
 		self.p1B1.config(state = DISABLED)
-		 
-		self.p1B2.config(state = NORMAL)
+
+		self.p1B2.config(state = NORMAL, foreground = "#05fa42")
 		self.p1B2.delete("1.0", END)
 		self.p1B2.insert(END, "status: " + cacheModelMatrix[1][2].getStatus() +"\naddress: " + cacheModelMatrix[1][2].getAddress() + "\ndata: " + cacheModelMatrix[1][2].getValue())
+		if(cacheModelMatrix[1][2].getStatus() == "E"):
+			self.p1B2.config(foreground = "red")
 		self.p1B2.config(state = DISABLED)
-		 
-		self.p1B3.config(state = NORMAL)
+
+		self.p1B3.config(state = NORMAL, foreground = "#05fa42")
 		self.p1B3.delete("1.0", END)
 		self.p1B3.insert(END, "status: " + cacheModelMatrix[1][3].getStatus() +"\naddress: " + cacheModelMatrix[1][3].getAddress() + "\ndata: " + cacheModelMatrix[1][3].getValue())
+		if(cacheModelMatrix[1][3].getStatus() == "E"):
+			self.p1B3.config(foreground = "red")
 		self.p1B3.config(state = DISABLED)
-		 
-		self.p2B0.config(state = NORMAL)
+
+		self.p2B0.config(state = NORMAL, foreground = "#05fa42")
 		self.p2B0.delete("1.0", END)
 		self.p2B0.insert(END, "status: " + cacheModelMatrix[2][0].getStatus() +"\naddress: " + cacheModelMatrix[2][0].getAddress() + "\ndata: " + cacheModelMatrix[2][0].getValue())
+		if(cacheModelMatrix[2][0].getStatus() == "E"):
+			self.p2B0.config(foreground = "red")
 		self.p2B0.config(state = DISABLED)
-		 
-		self.p2B1.config(state = NORMAL)
+
+		self.p2B1.config(state = NORMAL, foreground = "#05fa42")
 		self.p2B1.delete("1.0", END)
 		self.p2B1.insert(END, "status: " + cacheModelMatrix[2][1].getStatus() +"\naddress: " + cacheModelMatrix[2][1].getAddress() + "\ndata: " + cacheModelMatrix[2][1].getValue())
+		if(cacheModelMatrix[2][1].getStatus() == "E"):
+			self.p2B1.config(foreground = "red")
 		self.p2B1.config(state = DISABLED)
-		 
-		self.p2B2.config(state = NORMAL)
+
+		self.p2B2.config(state = NORMAL, foreground = "#05fa42")
 		self.p2B2.delete("1.0", END)
 		self.p2B2.insert(END, "status: " + cacheModelMatrix[2][2].getStatus() +"\naddress: " + cacheModelMatrix[2][2].getAddress() + "\ndata: " + cacheModelMatrix[2][2].getValue())
+		if(cacheModelMatrix[2][2].getStatus() == "E"):
+			self.p2B2.config(foreground = "red")
 		self.p2B2.config(state = DISABLED)
-		 
-		self.p2B3.config(state = NORMAL)
+
+		self.p2B3.config(state = NORMAL, foreground = "#05fa42")
 		self.p2B3.delete("1.0", END)
 		self.p2B3.insert(END, "status: " + cacheModelMatrix[2][3].getStatus() +"\naddress: " + cacheModelMatrix[2][3].getAddress() + "\ndata: " + cacheModelMatrix[2][3].getValue())
+		if(cacheModelMatrix[2][3].getStatus() == "E"):
+			self.p2B3.config(foreground = "red")
 		self.p2B3.config(state = DISABLED)
-		 
-		self.p3B0.config(state = NORMAL)
+
+		self.p3B0.config(state = NORMAL, foreground = "#05fa42")
 		self.p3B0.delete("1.0", END)
 		self.p3B0.insert(END, "status: " + cacheModelMatrix[3][0].getStatus() +"\naddress: " + cacheModelMatrix[3][0].getAddress() + "\ndata: " + cacheModelMatrix[3][0].getValue())
+		if(cacheModelMatrix[3][0].getStatus() == "E"):
+			self.p3B0.config(foreground = "red")
 		self.p3B0.config(state = DISABLED)
-		 
-		self.p3B1.config(state = NORMAL)
+
+		self.p3B1.config(state = NORMAL, foreground = "#05fa42")
 		self.p3B1.delete("1.0", END)
 		self.p3B1.insert(END, "status: " + cacheModelMatrix[3][1].getStatus() +"\naddress: " + cacheModelMatrix[3][1].getAddress() + "\ndata: " + cacheModelMatrix[3][1].getValue())
+		if(cacheModelMatrix[3][1].getStatus() == "E"):
+			self.p3B1.config(foreground = "red")
 		self.p3B1.config(state = DISABLED)
-		 
-		self.p3B2.config(state = NORMAL)
+
+		self.p3B2.config(state = NORMAL, foreground = "#05fa42")
 		self.p3B2.delete("1.0", END)
 		self.p3B2.insert(END, "status: " + cacheModelMatrix[3][2].getStatus() +"\naddress: " + cacheModelMatrix[3][2].getAddress() + "\ndata: " + cacheModelMatrix[3][2].getValue())
+		if(cacheModelMatrix[3][2].getStatus() == "E"):
+			self.p3B2.config(foreground = "red")
 		self.p3B2.config(state = DISABLED)
-		 
-		self.p3B3.config(state = NORMAL)
+
+		self.p3B3.config(state = NORMAL, foreground = "#05fa42")
 		self.p3B3.delete("1.0", END)
 		self.p3B3.insert(END, "status: " + cacheModelMatrix[3][3].getStatus() +"\naddress: " + cacheModelMatrix[3][3].getAddress() + "\ndata: " + cacheModelMatrix[3][3].getValue())
+		if(cacheModelMatrix[3][3].getStatus() == "E"):
+			self.p3B3.config(foreground = "red")
 		self.p3B3.config(state = DISABLED)
+
+
+	def missAlert(self, cacheId):
+		if(cacheId == 0):
+			self.p0B0.config(foreground = "red")
+			# self.p0B0.delete("1.0", END)
+			# self.p0B0.insert(END, "status: " + cacheModelMatrix[0][0].getStatus() +"\naddress: " + cacheModelMatrix[0][0].getAddress() + "\ndata: " + cacheModelMatrix[0][0].getValue())
+			# self.p0B0.config(state = DISABLED)
+
+		elif(cacheId == 1):
+			self.p0B1.config(foreground = "red")
+			# self.p0B1.delete("1.0", END)
+			# self.p0B1.insert(END, "status: " + cacheModelMatrix[0][1].getStatus() +"\naddress: " + cacheModelMatrix[0][1].getAddress() + "\ndata: " + cacheModelMatrix[0][1].getValue())
+			# self.p0B1.config(state = DISABLED)
+
+		elif(cacheId == 2):
+			self.p0B2.config(foreground = "red")
+			# self.p0B2.delete("1.0", END)
+			# self.p0B2.insert(END, "status: " + cacheModelMatrix[0][2].getStatus() +"\naddress: " + cacheModelMatrix[0][2].getAddress() + "\ndata: " + cacheModelMatrix[0][2].getValue())
+			# self.p0B2.config(state = DISABLED)
+
+		elif(cacheId == 3):
+			self.p0B3.config(foreground = "red")
+			# self.p0B3.delete("1.0", END)
+			# self.p0B3.insert(END, "status: " + cacheModelMatrix[0][3].getStatus() +"\naddress: " + cacheModelMatrix[0][3].getAddress() + "\ndata: " + cacheModelMatrix[0][3].getValue())
+			# self.p0B3.config(state = DISABLED)
+
+		elif(cacheId == 4):
+			self.p1B0.config(foreground = "red")
+			# self.p1B0.delete("1.0", END)
+			# self.p1B0.insert(END, "status: " + cacheModelMatrix[1][0].getStatus() +"\naddress: " + cacheModelMatrix[1][0].getAddress() + "\ndata: " + cacheModelMatrix[1][0].getValue())
+			# self.p1B0.config(state = DISABLED)
+
+		elif(cacheId == 5):
+			self.p1B1.config(foreground = "red")
+			# self.p1B1.delete("1.0", END)
+			# self.p1B1.insert(END, "status: " + cacheModelMatrix[1][1].getStatus() +"\naddress: " + cacheModelMatrix[1][1].getAddress() + "\ndata: " + cacheModelMatrix[1][1].getValue())
+			# self.p1B1.config(state = DISABLED)
+
+		elif(cacheId == 6):
+			self.p1B2.config(foreground = "red")
+			# self.p1B2.delete("1.0", END)
+			# self.p1B2.insert(END, "status: " + cacheModelMatrix[1][2].getStatus() +"\naddress: " + cacheModelMatrix[1][2].getAddress() + "\ndata: " + cacheModelMatrix[1][2].getValue())
+			# self.p1B2.config(state = DISABLED)
+
+		elif(cacheId == 7):
+			self.p1B3.config(foreground = "red")
+			# self.p1B3.delete("1.0", END)
+			# self.p1B3.insert(END, "status: " + cacheModelMatrix[1][3].getStatus() +"\naddress: " + cacheModelMatrix[1][3].getAddress() + "\ndata: " + cacheModelMatrix[1][3].getValue())
+			# self.p1B3.config(state = DISABLED)
+
+		elif(cacheId == 8):
+			self.p2B0.config(foreground = "red")
+			# self.p2B0.delete("1.0", END)
+			# self.p2B0.insert(END, "status: " + cacheModelMatrix[2][0].getStatus() +"\naddress: " + cacheModelMatrix[2][0].getAddress() + "\ndata: " + cacheModelMatrix[2][0].getValue())
+			# self.p2B0.config(state = DISABLED)
+
+		elif(cacheId == 9):
+			self.p2B1.config(foreground = "red")
+			# self.p2B1.delete("1.0", END)
+			# self.p2B1.insert(END, "status: " + cacheModelMatrix[2][1].getStatus() +"\naddress: " + cacheModelMatrix[2][1].getAddress() + "\ndata: " + cacheModelMatrix[2][1].getValue())
+			# self.p2B1.config(state = DISABLED)
+
+		elif(cacheId == 10):
+			self.p2B2.config(foreground = "red")
+			# self.p2B2.delete("1.0", END)
+			# self.p2B2.insert(END, "status: " + cacheModelMatrix[2][2].getStatus() +"\naddress: " + cacheModelMatrix[2][2].getAddress() + "\ndata: " + cacheModelMatrix[2][2].getValue())
+			# self.p2B2.config(state = DISABLED)
+
+		elif(cacheId == 11):
+			self.p2B3.config(foreground = "red")
+			# self.p2B3.delete("1.0", END)
+			# self.p2B3.insert(END, "status: " + cacheModelMatrix[2][3].getStatus() +"\naddress: " + cacheModelMatrix[2][3].getAddress() + "\ndata: " + cacheModelMatrix[2][3].getValue())
+			# self.p2B3.config(state = DISABLED)
+
+		elif(cacheId == 12):
+			self.p3B0.config(foreground = "red")
+			# self.p3B0.delete("1.0", END)
+			# self.p3B0.insert(END, "status: " + cacheModelMatrix[3][0].getStatus() +"\naddress: " + cacheModelMatrix[3][0].getAddress() + "\ndata: " + cacheModelMatrix[3][0].getValue())
+			# self.p3B0.config(state = DISABLED)
+
+		elif(cacheId == 13):
+			self.p3B1.config(foreground = "red")
+			# self.p3B1.delete("1.0", END)
+			# self.p3B1.insert(END, "status: " + cacheModelMatrix[3][1].getStatus() +"\naddress: " + cacheModelMatrix[3][1].getAddress() + "\ndata: " + cacheModelMatrix[3][1].getValue())
+			# self.p3B1.config(state = DISABLED)
+
+		elif(cacheId == 14):
+			self.p3B2.config(foreground = "red")
+			# self.p3B2.delete("1.0", END)
+			# self.p3B2.insert(END, "status: " + cacheModelMatrix[3][2].getStatus() +"\naddress: " + cacheModelMatrix[3][2].getAddress() + "\ndata: " + cacheModelMatrix[3][2].getValue())
+			# self.p3B2.config(state = DISABLED)
+
+		elif(cacheId == 15):
+			self.p3B3.config(foreground = "red")
+			# self.p3B3.delete("1.0", END)
+			# self.p3B3.insert(END, "status: " + cacheModelMatrix[3][3].getStatus() +"\naddress: " + cacheModelMatrix[3][3].getAddress() + "\ndata: " + cacheModelMatrix[3][3].getValue())
+			# self.p3B3.config(state = DISABLED)
